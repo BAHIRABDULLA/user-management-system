6dafce8d7f8ce506a97c2529d1da5702caa62ab5
Bahir Abdulla <bahirk880@gmail.com>
2024-07-09 16:02:35 +0530

    update header and privateRoute added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router , Route,Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Pages/user/Home'
 import SignIn from './Pages/user/SignIn'
 import SignUp from './Pages/user/SignUp'
 import Profile from './Pages/user/Profile'
 import Header from './Components/Header'
+import PrivateRoute from './Components/PrivateRoute'
 
 
 const App = () => {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/signin' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+
       </Routes>
     </Router>
   )
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+    const {currentUser} = useSelector(state=>state.user)
     return (
         <div className='bg-zinc-300'>
             <div className='flex justify-between p-7'>
                 <h1 className='font-bold text-red-800 max-w-6xl items-center'>User Management System</h1>
                 <ul className='flex gap-4'>
                     <Link to='/'><li>Home</li></Link>
-                    <Link to='/signin'><li>Sign In</li></Link>
+                    <Link to='/signin'>
+                    {currentUser?(<img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full'/>):(<li>Sign In</li>)}</Link>
                     <Link to='/profile'><li>Profile</li></Link>
                     
                     
